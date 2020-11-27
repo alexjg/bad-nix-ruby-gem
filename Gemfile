442b30fbf580c2ba3e49303ec5722f0e45517ed1
Alex Good <alex.good@mypulse.ai>
2020-11-27 13:02:03 +0000

    Initial import

---
+++ Gemfile
@@ -0,0 +1,1 @@
+gem "getsource", "0.2.2", git: "https://github.com/alexjg/getsource.git"
---
+++ gemset.nix
@@ -0,0 +1,14 @@
+{
+  getsource = {
+    groups = ["default"];
+    platforms = [];
+    source = {
+      fetchSubmodules = false;
+      rev = "43417d97fb1391548659c513c257cf019e409d9b";
+      sha256 = "127gz30bhlplki20m97ryfjrz7c6d9c4p03yz5a2x7kpw60g8d7j";
+      type = "git";
+      url = "https://github.com/alexjg/getsource.git";
+    };
+    version = "0.2.2";
+  };
+}
